75f75e9aa50742dd466e8a25b7c328d9b0d47da0
Abdurazzoq3003 <aabdurazzoq80@gmail.com>
2024-08-29 12:53:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+// let kocha = 'issiq';
+
+// if (kocha === 'issiq') {
+//   console.log('Futbolga kiyib oling chunki kocha issiq');
+// } else if (kocha === 'sovuq') {
+//   console.log('Qalinroq kiyinib oling');
+// } else if (kocha === 'qor') {
+//   console.log('Juda sovuq boladi');
+// } else {
+//   console.log('bu shart togri berilmagan');
+// }
+
+// let age = 23;
+
+// if (age > 6) {
+//   console.log('Siz maktab yoshidasiz');
+// } else {
+//   console.log('Siz maktab yoshiga yetmagansiz');
+// }
+
+//String methods
+
+let ism = "Avazbek va Rashodbek";
+
+console.log(ism);
+console.log(ism.length);
+console.log(ism.trim());
+console.log(ism.slice(0, 8));
+console.log(ism.includes("Ismoil"));
+console.log(ism.indexOf("r"));
+console.log(ism.toLowerCase());
+console.log(ism.toUpperCase());
